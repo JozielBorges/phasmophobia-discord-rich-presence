558ac9ad77d8fcb8fa7dc6491446cb1e303ea051
Knuckles <34248081+JozielBorges@users.noreply.github.com>
2020-10-14 19:00:58 -0300

    Source Code

---
+++ Phasmophobia_RPC/Phasmophobia_RPC/MyMod.cs
@@ -0,0 +1,134 @@
+﻿using MelonLoader;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Phasmophobia_RPC {
+    public class MyMod : MelonMod {
+        public Discord.Discord discord;
+        public static ServerManager serverManager;
+        public static bool loaded = false;
+
+        public static long timeStamp = 0;
+        public static string roomNow, roomOld,mapa;
+
+        public override void OnApplicationStart() {
+            MelonCoroutines.Start(TakeGameObjects());
+
+            discord = new Discord.Discord(765944720882270211, (System.UInt64)Discord.CreateFlags.Default);
+        }
+        public override void OnLevelWasInitialized(int level) {
+            if (level == 1) {
+                loaded = true;
+                roomNow = "Menu";
+
+                var activityManager = discord.GetActivityManager();
+                var activity = new Discord.Activity {
+                    State = "",
+                    Details = roomNow,
+                    Timestamps =
+           {
+                Start = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds,
+            },
+                    Assets =
+           {
+                LargeImage = "icon",
+                LargeText = "By Knuckles#4442"
+            }, };
+                activityManager.UpdateActivity(activity, (res) => {
+                    if (res == Discord.Result.Ok) {
+                        MelonLogger.Log("Everything is fine!");
+                    }
+                });
+            }
+        }
+        public override void OnUpdate() {
+
+            if (PhotonNetwork.inRoom && PhotonNetwork.room.Name != roomOld) {
+                roomNow = PhotonNetwork.room.Name;
+                roomOld = roomNow;
+                UpdateActivity(discord, IsRoomPrivate(), RoomName(), (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+            } else if (PhotonNetwork.inRoom && RoomName() != mapa) {
+                mapa = RoomName();
+                UpdateActivity(discord, IsRoomPrivate(), RoomName(), (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+            }
+
+
+            discord.RunCallbacks();
+
+        }
+
+        public string RoomName() {
+            string SceneRoom = SceneManager.GetActiveScene().name.Replace('_', ' ');
+            if (SceneRoom == "Menu New") {
+                return "Menu";
+            }
+            return SceneRoom;
+        }
+        public string IsRoomPrivate() {
+            if (PhotonNetwork.room.IsVisible) {
+                return "Public";
+            } else {
+                return "Private";
+            }
+        }
+        public IEnumerator TakeGameObjects() {
+            serverManager = UnityEngine.Object.FindObjectOfType<ServerManager>();
+            yield return new WaitForSeconds(0.1f);
+            yield return null;
+            yield break;
+        }
+
+        static void UpdateActivity(Discord.Discord discord, string state, string Details, long time) {
+            if (loaded) {
+                var activityManager = discord.GetActivityManager();
+                var lobbyManager = discord.GetLobbyManager();
+                var activity = new Discord.Activity {
+                    State = state,
+                    Details = Details,
+                    Timestamps =
+                    {
+                Start = time,
+            },
+                    Assets =
+                    {
+                LargeImage = "icon",
+                LargeText = "By Knuckles#4442",
+            },
+                    Party = {
+               Id = "knuckles",
+               Size = {
+                    CurrentSize = PhotonNetwork.playerList.Length,
+                    MaxSize = 4,
+                },
+            },
+                    Secrets = {
+                Join = null,
+            },
+                    Instance = true,
+                };
+
+                activityManager.UpdateActivity(activity, result => {
+
+                    // Send an invite to another user for this activity.
+                    // Receiver should see an invite in their DM.
+                    // Use a relationship user's ID for this.
+                    // activityManager
+                    //   .SendInvite(
+                    //       364843917537050624,
+                    //       Discord.ActivityActionType.Join,
+                    //       "",
+                    //       inviteResult =>
+                    //       {
+                    //           Console.WriteLine("Invite {0}", inviteResult);
+                    //       }
+                    //   );
+                });
+            }
+        }
+
+
+        
+    }
+}
---
+++ Phasmophobia_RPC/Phasmophobia_RPC/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using Phasmophobia_RPC;
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("Phasmophobia_RPC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Phasmophobia_RPC")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(MyMod),"Discord RPC","0.0.1", "Knuckles#4442", "https://github.com/JozielBorges/phasmophobia-discord-rich-presence")]
+[assembly: MelonGame("Kinetic Games", "Phasmophobia")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("b4c90036-33e6-45af-905b-3245b18e44c5")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
