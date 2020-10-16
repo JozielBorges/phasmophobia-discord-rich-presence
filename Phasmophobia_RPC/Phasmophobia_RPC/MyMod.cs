263a32772ce05c58b825b391acc3d67605206ff9
Knuckles <34248081+JozielBorges@users.noreply.github.com>
2020-10-16 13:20:09 -0300

    Update MyMod.cs
    !Fixed some issues!
    
    FIXED
     After a job, rpc don't show room info.
     After leave the job, rpc continuous showing "private".
     Code adjustments.

--- Phasmophobia_RPC/Phasmophobia_RPC/MyMod.cs
+++ Phasmophobia_RPC/Phasmophobia_RPC/MyMod.cs
@@ -7,10 +7,11 @@ using UnityEngine.SceneManagement;
 namespace Phasmophobia_RPC {
     public class MyMod : MelonMod {
         public Discord.Discord discord;
-        public static bool loaded = false;
 
         public static long timeStamp = 0;
-        public static string roomNow, roomOld,mapa;
+        public static string roomNow, roomOld, mapa;
+
+        int playerSize;
 
         public override void OnApplicationStart() {
 
@@ -18,7 +19,6 @@ namespace Phasmophobia_RPC {
         }
         public override void OnLevelWasInitialized(int level) {
             if (level == 1) {
-                loaded = true;
                 roomNow = "Menu";
 
                 var activityManager = discord.GetActivityManager();
@@ -36,23 +36,32 @@ namespace Phasmophobia_RPC {
             }, };
                 activityManager.UpdateActivity(activity, (res) => {
                     if (res == Discord.Result.Ok) {
-                        MelonLogger.Log("Everything is fine!");
                     }
                 });
+            } else if(level >0){
+                roomNow = PhotonNetwork.room.Name;
+                roomOld = roomNow;
+                UpdateActivity(discord, IsRoomPrivate(), RoomName(), (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds, playerSize);
             }
         }
         public override void OnUpdate() {
+            if (PhotonNetwork.inRoom) {
+                playerSize = PhotonNetwork.playerList.Length;
+            }
+
+            if(RoomName() != mapa) {
+                mapa = RoomName();
+            }
 
             if (PhotonNetwork.inRoom && PhotonNetwork.room.Name != roomOld) {
                 roomNow = PhotonNetwork.room.Name;
                 roomOld = roomNow;
-                UpdateActivity(discord, IsRoomPrivate(), RoomName(), (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
-            } else if (PhotonNetwork.inRoom && RoomName() != mapa) {
-                mapa = RoomName();
-                UpdateActivity(discord, IsRoomPrivate(), RoomName(), (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+                UpdateActivity(discord, IsRoomPrivate(), RoomName(), (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds, playerSize);
+            } 
+
+            if(PhotonNetwork.inRoom && PhotonNetwork.room.Name == roomOld && RoomName() != mapa) {
+                UpdateActivity(discord, IsRoomPrivate(), RoomName(), (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds, playerSize);
             }
-
-
             discord.RunCallbacks();
 
         }
@@ -72,55 +81,51 @@ namespace Phasmophobia_RPC {
             }
         }
 
-        static void UpdateActivity(Discord.Discord discord, string state, string Details, long time) {
-            if (loaded) {
-                var activityManager = discord.GetActivityManager();
-                var lobbyManager = discord.GetLobbyManager();
-                var activity = new Discord.Activity {
-                    State = state,
-                    Details = Details,
-                    Timestamps =
-                    {
+        static void UpdateActivity(Discord.Discord discord, string state, string Details, long time, int size) {
+            var activityManager = discord.GetActivityManager();
+            var lobbyManager = discord.GetLobbyManager();
+            var activity = new Discord.Activity {
+                State = state,
+                Details = Details,
+                Timestamps =
+                {
                 Start = time,
             },
-                    Assets =
-                    {
+                Assets =
+                {
                 LargeImage = "icon",
                 LargeText = "By Knuckles#4442",
             },
-                    Party = {
+                Party = {
                Id = "knuckles",
                Size = {
-                    CurrentSize = PhotonNetwork.playerList.Length,
+                    CurrentSize = size,
                     MaxSize = 4,
                 },
             },
-                    Secrets = {
+                Secrets = {
                 Join = null,
             },
-                    Instance = true,
-                };
+                Instance = true,
+            };
 
-                activityManager.UpdateActivity(activity, result => {
+            activityManager.UpdateActivity(activity, result => {
 
-                    // Send an invite to another user for this activity.
-                    // Receiver should see an invite in their DM.
-                    // Use a relationship user's ID for this.
-                    // activityManager
-                    //   .SendInvite(
-                    //       364843917537050624,
-                    //       Discord.ActivityActionType.Join,
-                    //       "",
-                    //       inviteResult =>
-                    //       {
-                    //           Console.WriteLine("Invite {0}", inviteResult);
-                    //       }
-                    //   );
-                });
-            }
+                // Send an invite to another user for this activity.
+                // Receiver should see an invite in their DM.
+                // Use a relationship user's ID for this.
+                // activityManager
+                //   .SendInvite(
+                //       364843917537050624,
+                //       Discord.ActivityActionType.Join,
+                //       "",
+                //       inviteResult =>
+                //       {
+                //           Console.WriteLine("Invite {0}", inviteResult);
+                //       }
+                //   );
+            });
+
         }
-
-
-        
     }
 }
